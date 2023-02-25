ad1d0a2255faa2ccc643c5a13e75e60a56bb5d62
yutingli <yutingli0111@gmail.com>
2023-02-25 20:55:39 +0800

    Change javascript to text/javascript

--- blockAds.js
+++ blockAds.js
@@ -1,26 +1,21 @@
 let url = $request.url;
 if (url.indexOf("missav") != -1) {
-
     let window_open_reg = 'window.open';
     let window_open_str = '';
     let reg = '<head>';
     let str = '<head>\
-    <link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" />\
-    <script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+    <script type="text/javascript" async="async" src="https://raw.githubusercontent.com/yutingli11/scripts/main/userScript.js"></script>'
     let body = $response.body.replace(reg, str).replace(window_open_reg, window_open_str);
     let headers = $response.headers;
     headers['Content-Security-Policy'] = "child-src	'self'";
     $done({ headers, body, url })
 
 } else {
-
     let reg = '<head>';
     let str = '<head>\
-    <link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" />\
-    <script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+    <script type="text/javascript" async="async" src="https://raw.githubusercontent.com/yutingli11/scripts/main/userScript.js"></script>'
     let body = $response.body.replace(reg, str);
     let headers = $response.headers;
     headers['Content-Security-Policy'] = '';
     $done({ headers, body, url })
-
 }
