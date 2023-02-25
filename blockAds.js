d92bbb0e6d55afa935e14021d2a4f5c8723dce20
yutingli <yutingli0111@gmail.com>
2023-02-25 21:03:55 +0800

    Update blockAds

--- blockAds.js
+++ blockAds.js
@@ -1,9 +1,11 @@
 let url = $request.url;
 if (url.indexOf("missav") != -1) {
+
     let window_open_reg = 'window.open';
     let window_open_str = '';
     let reg = '<head>';
     let str = '<head>\
+    <link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" />\
     <script type="text/javascript" async="async" src="https://raw.githubusercontent.com/yutingli11/scripts/main/userScript.js"></script>'
     let body = $response.body.replace(reg, str).replace(window_open_reg, window_open_str);
     let headers = $response.headers;
@@ -11,11 +13,14 @@ if (url.indexOf("missav") != -1) {
     $done({ headers, body, url })
 
 } else {
+
     let reg = '<head>';
     let str = '<head>\
+    <link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" />\
     <script type="text/javascript" async="async" src="https://raw.githubusercontent.com/yutingli11/scripts/main/userScript.js"></script>'
     let body = $response.body.replace(reg, str);
     let headers = $response.headers;
     headers['Content-Security-Policy'] = '';
     $done({ headers, body, url })
+
 }
